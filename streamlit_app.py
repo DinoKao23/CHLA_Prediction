c7d636f4b43c11e37b042e9f4591766b9aadfe84
Tsai Lieh Kao <144483922+DinoKao23@users.noreply.github.com>
2024-04-22 23:31:21 -0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,60 +1,77 @@
 # python -m streamlit run hello.py
 
+import sklearn
 import streamlit as st
 import pandas as pd
 import datetime
-import sklearn
 import pickle
-import logging
 
-# Configure logging
-logging.basicConfig(filename='./app.log', level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
-
+def load_data(file_path):
+    return pd.read_csv(file_path)
 model = pickle.load(open('random_forest.pkl', 'rb'))
-
 today = datetime.datetime.today()
 date_string = datetime.datetime(2000, 1, 1)
 
 
 def main():
-    st.set_page_config(page_title="CHIA Prediction")
+    st.set_page_config(page_title="CHLA Prediction", layout= 'wide', page_icon= '🏥')
     st.title("Patient Showup Prediction: ")
 
-    appt_date = st.date_input(label = "Appointment Date", value = today, min_value= date_string)
-    book_date = st.date_input(label = "Booking Date", value = today, min_value= date_string)
-    lead_time = st.text_input("Lead time", 0)
-    total_no_show = st.text_input("How many times this patient hasn't show up in the past?",0)
-    total_success = st.text_input("How many times this patient has show up for their appointment?",0)
-    total_cancel = st.text_input("How many times this patient has cancel for their appointment?",0)
-    total_reschedule = st.text_input("How many times this patient has reschedule for their appointment?",0)
+    hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+    st.markdown(hide_default_format, unsafe_allow_html=True)
 
-    if st.button("Predict"):
-        data = {
-        'appt_date': appt_date,
-        'book_date': book_date,
-        'lead_time': int(lead_time),
-        'total_no_show': int(total_no_show),
-        'total_success': int(total_success),
-        'total_cancel': int(total_cancel),
-        'total_reschedule': int(total_reschedule)}
+    df = load_data("CHLA_clean_data_until_2023.csv")
+    df['APPT_DATE'] = pd.to_datetime(df['APPT_DATE'])
 
-        predict_df = pd.DataFrame([data])
+    
+    default_start_date = df['APPT_DATE'].min()
+    default_end_date = df['APPT_DATE'].max()
 
-        predict_df['appt_date'] = pd.to_datetime(predict_df['appt_date'])
-        predict_df['book_date'] = pd.to_datetime(predict_df['book_date'])
+    # Create columns
+    c1, c2 = st.columns([1, 1])
 
-        predict_df['NUM_OF_MONTH'] = predict_df['appt_date'].dt.month
+    # Within c1
+    with c1:
+    # Date input for start date
+        start_datetime = st.date_input("Choose Start Date", min_value=default_start_date, max_value=default_end_date, value=default_start_date)
+
+# Within c2
+    with c2:
+        # Date input for end date
+        end_datetime = st.date_input("Choose End Date", min_value=default_start_date, max_value=default_end_date, value=default_end_date)
+
+    start_datetime = pd.to_datetime(start_datetime)
+    end_datetime = pd.to_datetime(end_datetime)
+
+    clinic = st.selectbox("Select an Clinic", df['CLINIC'].unique())
+
+    if st.button("Predict"):    
+        filtered_df = df[(df['APPT_DATE'] >= start_datetime) & (df['APPT_DATE'] <= end_datetime)]
+        clinic_df = filtered_df[filtered_df['CLINIC'] == clinic]
+
+        predict_df = clinic_df[['APPT_DATE', 'BOOK_DATE', 'LEAD_TIME', 'TOTAL_NUMBER_OF_NOSHOW', 'TOTAL_NUMBER_OF_SUCCESS_APPOINTMENT', 'TOTAL_NUMBER_OF_CANCELLATIONS', 'TOTAL_NUMBER_OF_RESCHEDULED']]
+
+        predict_df['APPT_DATE'] = pd.to_datetime(predict_df['APPT_DATE'])
+        predict_df['BOOK_DATE'] = pd.to_datetime(predict_df['BOOK_DATE'])
+
+        predict_df['NUM_OF_MONTH'] = predict_df['APPT_DATE'].dt.month
 
         # Now, all your features should be numerical, and you can attempt prediction
-        features_list = predict_df.drop(['appt_date','book_date' ],axis = 1).values
-        prediction = model.predict(features_list)
-        
-        if prediction == 1:
-            text = "Patient will not show up"
-        else:
-            text = "Patient will show up"
+        features_list = predict_df.drop(['APPT_DATE','BOOK_DATE'],axis = 1).values
+        try:
+            prediction = model.predict(features_list)
+            predict_df['prediction'] = prediction
+            prediction_text_map = { 1: "Patient will not show up",0: "Patient will show up"}
+            clinic_df['prediction_text'] = clinic_df['prediction'].map(prediction_text_map)
+        except ValueError as e:
+            st.error("There are no clients in this range, please select other ranges.")
 
-        st.success(text)
+        st.write(clinic_df)
         
 if __name__=='__main__':
     main()
